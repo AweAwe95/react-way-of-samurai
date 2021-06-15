f444476045c42abacd918c1e6ac600d7690bea0d
AweAwe95 <vitalikarach95@gmail.com>
2021-06-16 00:26:29 +0300

    import-export of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Technologies} from "./components/Technologies";
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Technologies/>
-        </div>
-    );
-}
-
-function Header() {
-    return (
-        <div>
-            <ul>
-                <a href={'#'}>Home </a>
-                <a href={'#'}>News Feed </a>
-                <a href={'#'}>Messages </a>
-            </ul>
-        </div>
-    );
-}
-
-function Technologies() {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div>
+            <ul>
+                <a href={'#'}>Home </a>
+                <a href={'#'}>News Feed </a>
+                <a href={'#'}>Messages </a>
+            </ul>
+        </div>
+    );
+}
